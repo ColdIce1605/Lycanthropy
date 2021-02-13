4a58ff6178f1d74d3bdbabae54b19bb4e8d86d39
James R <71466513+ColdIce1605@users.noreply.github.com>
2021-02-13 13:45:36 -0600

    init

---
+++ src/main/java/net/coldice/lycanthropy/Lycanthropy.java
@@ -0,0 +1,5 @@
+package net.coldice.lycanthropy;
+
+public class Lycanthropy {
+
+}
---
+++ src/main/java/net/coldice/lycanthropy/client/LycanthropyClient.java
@@ -0,0 +1,5 @@
+package net.coldice.lycanthropy.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class LycanthropyClient {
+}
